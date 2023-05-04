8ca9a4c76e368661130d0870efaf47344df9f162
Simon Kobler <simon@kobler.me>
2023-05-04 16:36:56 +0200

    Bugfixes

--- projects/ngx-gdpr-cookie-consent/src/public-api.ts
+++ projects/ngx-gdpr-cookie-consent/src/public-api.ts
@@ -6,3 +6,4 @@ export * from './lib/service/ngx-gdpr-cookie-consent.service';
 export * from './lib/components/ngx-gdpr-cookie-consent/ngx-gdpr-cookie-consent.component';
 export * from './lib/components/ngx-gdpr-cookie-container/ngx-gdpr-cookie-container.component';
 export * from './lib/ngx-gdpr-cookie-consent.module';
+export * from './lib/model/common-types';
